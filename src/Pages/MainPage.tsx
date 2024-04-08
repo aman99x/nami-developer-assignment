eca2fd91881c8370b6fed86fd61ac979cb7db463
aman99x <aman88sh@gmail.com>
2024-04-08 23:09:44 +0530

    update

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,20 +1,43 @@
-// import MobileFooter from "../components/MobileFooter";
-// import MobileHeader from "../components/MobileHeader";
+import { useState, useEffect } from 'react';
+import MobileFooter from "../components/MobileFooter";
+import MobileHeader from "../components/MobileHeader";
 import SideNav from "../components/SideNav";
 import TaskCard from "../components/TaskCard";
 import TopNav from "../components/TopNav";
 
-
 function MainPage() {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    const updateMedia = () => {
+      setIsMobile(window.innerWidth < 768); 
+    };
+
+    updateMedia();
+    window.addEventListener('resize', updateMedia);
+
+    return () => {
+      window.removeEventListener('resize', updateMedia);
+    };
+  }, []);
+
   return (
     <>
-      {/* <MobileHeader/> */}
-      <TopNav/>
-      <div className="flex">   
-      <SideNav/>
-      <TaskCard/>
-    </div>
-    {/* <MobileFooter/> */}
+      {isMobile ? (
+        <>
+          <MobileHeader/> 
+          <TaskCard/>
+          <MobileFooter/>
+        </>
+      ) : (
+        <>
+          <TopNav/>
+          <div className="flex">   
+            <SideNav/>
+            <TaskCard/>
+          </div>
+        </>
+      )}
     </>
   );
 }
