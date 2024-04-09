7bf1f0a147ef97406d89e85eb5afb72b3e6498f5
aman99x <aman88sh@gmail.com>
2024-04-09 11:07:23 +0530

    update

--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -1,91 +1,6 @@
 export default function MobileHeader() {
     return (
       <div className="bg-[#FFFFFF] flex flex-col p-[2.7px_0_0_0] box-sizing-border">
-        <div className="relative m-[0_5.3px_21.7px_4px] flex flex-row justify-between w-[calc(100%_-_9.3px)] box-sizing-border">
-          <div className="relative m-[0.3px_0_2.2px_0] flex flex-row justify-between w-[74.1px] h-[fit-content] box-sizing-border">
-            <img className="w-[21.1px] h-[14.1px]" />
-            <div className="m-[2px_0_2px_0] flex flex-row justify-center w-[14.3px] h-[10.1px] box-sizing-border">
-              <img className="w-[14.3px] h-[10.1px]" />
-            </div>
-            <span className="absolute right-[19.5px] bottom-[-1.9px] break-words font-['SF_Pro_Text','Roboto_Condensed'] font-normal text-[12.1px] tracking-[-0.1px] leading-[1.333] text-[#FFFFFF]">
-            Figma
-            </span>
-          </div>
-          <div className="m-[0_0_0.6px_0] inline-block break-words font-['SF_Pro_Text','Roboto_Condensed'] font-semibold text-[12.1px] leading-[1.333] text-[#FFFFFF]">
-          9:41 AM
-          </div>
-          <div className="m-[0.6px_0_0_0] flex flex-row box-sizing-border">
-            <div className="m-[2.2px_6.1px_4.6px_0] flex flex-row justify-center w-[10.1px] h-[9.2px] box-sizing-border">
-              <img className="w-[10.1px] h-[9.2px]" />
-            </div>
-            <div className="m-[0.9px_5.7px_3.3px_0] flex flex-row justify-center w-[7px] h-[11.8px] box-sizing-border">
-              <img className="w-[7px] h-[11.8px]" />
-            </div>
-            <span className="m-[0_7.9px_0_0] break-words font-['SF_Pro_Text','Roboto_Condensed'] font-normal text-[12.1px] leading-[1.333] text-[#FFFFFF]">
-            100%
-            </span>
-            <img className="m-[1.2px_0_3.2px_0] w-[26.7px] h-[11.6px]" />
-          </div>
-        </div>
-        <div className="m-[0_0_21px_20px] flex flex-col w-[502px] box-sizing-border">
-          <div className="m-[0_0_24px_0] flex flex-row justify-between self-start w-[335px] box-sizing-border">
-            <div className="m-[6px_10px_6px_0] inline-block w-[216.3px] break-words font-['Encode_Sans_Expanded'] font-[var(--texttitle-text,600)] text-[16px] text-[#1A0A02]">
-            Tasks of the Day
-            </div>
-            <div className="flex flex-row w-[108.7px] box-sizing-border">
-              <div className="m-[7.3px_23.3px_7.3px_0] flex flex-row justify-center w-[25px] h-[25px] box-sizing-border">
-                <img src="https://t3.ftcdn.net/jpg/05/99/84/86/360_F_599848646_MdK7wVDJQZygObyKEahVkOuhh0IGdTt3.jpg" className="w-[25px] h-[25px]" />
-              </div>
-              <div className="m-[6.3px_20px_6.3px_0] flex flex-row justify-center w-[16px] h-[19.5px] box-sizing-border">
-                <img src="https://w7.pngwing.com/pngs/141/637/png-transparent-computer-icons-notifications-cdr-area-sound-icon-thumbnail.png" className="w-[16px] h-[19.5px]" />
-              </div>
-              <div className="rounded-[16px] bg-[url('assets/images/Ellipse185.png')] bg-[50%_50%] bg-cover bg-no-repeat w-[32px] h-[32px]">
-              </div>
-            </div>
-          </div>
-          <div className="shadow-[0px_1px_4px_0px_rgba(0,0,0,0.1)] rounded-r-[12px] bg-[#F7F7F7] m-[0_0_24px_0] flex flex-row justify-between self-start p-[10.5px_28.2px_10.5px_24px] w-[335px] box-sizing-border -ml-[8%] ">
-            <span className="m-[0_7.5px_0_0] w-[263.7px] break-words font-['Encode_Sans_Expanded'] font-[var(--textsecondary-title-text-1,600)] text-[12px] text-[#CC2610]">
-            Negative notification type description
-            </span>
-            <div className="m-[1.7px_0_1.7px_0] flex flex-row justify-center w-[11.7px] h-[11.7px] box-sizing-border">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgs1_D_RsTC-uPX430cPiV7Zm70TnvtknvTsSQLrSoNg&s" className="w-[11.7px] h-[11.7px]" />
-            </div>
-          </div>
-          <div className="flex flex-row justify-between w-[100%] box-sizing-border">
-            <div className="rounded-[20px] border-[1px_solid_#B8B8B8] m-[0_8px_0_0] flex flex-row justify-center p-[5.5px_7.5px_5.5px_7px] grow basis-[100px] box-sizing-border">
-              <span className="break-words font-['Encode_Sans_Expanded'] font-[var(--buttonbutton-text-secondary,500)] text-[12px] text-[#767676]">
-              Not Accepted
-              </span>
-            </div>
-            <div className="rounded-[20px] border-[1px_solid_#B8B8B8] m-[0_8px_0_0] flex flex-row justify-center p-[5.5px_7.8px_5.5px_7px] grow basis-[70px] box-sizing-border">
-              <span className="break-words font-['Encode_Sans_Expanded'] font-[var(--buttonbutton-text-secondary,500)] text-[12px] text-[#767676]">
-              Ongoing
-              </span>
-            </div>
-            <div className="rounded-[20px] border-[1px_solid_#B8B8B8] m-[0_8px_0_0] flex flex-row justify-center p-[5.5px_7.5px_5.5px_7px] grow basis-[80px] box-sizing-border">
-              <span className="break-words font-['Encode_Sans_Expanded'] font-[var(--buttonbutton-text-secondary,500)] text-[12px] text-[#767676]">
-              Scheduled
-              </span>
-            </div>
-            <div className="rounded-[20px] border-[1px_solid_#B8B8B8] m-[0_8px_0_0] flex flex-row justify-center p-[5.5px_7.9px_5.5px_7px] grow basis-[83px] box-sizing-border">
-              <span className="break-words font-['Encode_Sans_Expanded'] font-[var(--buttonbutton-text-secondary,500)] text-[12px] text-[#767676]">
-              Completed
-              </span>
-            </div>
-            <div className="flex flex-row grow basis-[137px] box-sizing-border">
-              <div className="rounded-[20px] border-[1px_solid_#B8B8B8] m-[0_8px_0_0] flex flex-row justify-center p-[5.5px_7.9px_5.5px_7px] box-sizing-border">
-                <span className="break-words font-['Encode_Sans_Expanded'] font-[var(--buttonbutton-text-secondary,500)] text-[12px] text-[#767676]">
-                Delayed
-                </span>
-              </div>
-              <div className="rounded-[20px] border-[1px_solid_#B8B8B8] flex flex-row justify-center p-[5.5px_7.4px_5.5px_7px] box-sizing-border">
-                <span className="break-words font-['Encode_Sans_Expanded'] font-[var(--buttonbutton-text-secondary,500)] text-[12px] text-[#767676]">
-                Ontime
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
         <div className="bg-[#FFFFFF] absolute left-[0px] top-[0px] right-[0px] flex flex-row justify-between p-[2.7px_5.3px_0.7px_4px] box-sizing-border">
           <div className="relative m-[0.3px_0_2.2px_0] flex flex-row justify-between w-[74.1px] h-[fit-content] box-sizing-border">
             <img src="https://banner2.cleanpng.com/20180701/lig/kisspng-signal-strength-in-telecommunications-mobile-phone-5b3967a569e9e9.8227481715304887414338.jpg" className="w-[21.1px] h-[14.1px]" />
@@ -110,6 +25,31 @@ export default function MobileHeader() {
             100%
             </span>
             <img src="https://cdn-icons-png.flaticon.com/512/664/664886.png" className="m-[1.2px_0_3.2px_0] w-[20px] h-[12px]" />
+          </div>
+        </div>
+        <div className="m-[0_0_21px_20px] flex flex-col w-[502px] box-sizing-border">
+          <div className="m-[0_0_24px_0] flex flex-row justify-between self-start w-[335px] box-sizing-border mt-[8%]">
+            <div className="m-[6px_10px_6px_0] inline-block w-[216.3px] break-words font-['Encode_Sans_Expanded'] font-[var(--texttitle-text,600)] text-[16px] text-[#1A0A02]">
+            Tasks of the Day
+            </div>
+            <div className="flex flex-row w-[108.7px] box-sizing-border">
+              <div className="m-[7.3px_23.3px_7.3px_0] flex flex-row justify-center w-[25px] h-[25px] box-sizing-border">
+                <img src="https://t3.ftcdn.net/jpg/05/99/84/86/360_F_599848646_MdK7wVDJQZygObyKEahVkOuhh0IGdTt3.jpg" className="w-[25px] h-[25px]" />
+              </div>
+              <div className="m-[6.3px_20px_6.3px_0] flex flex-row justify-center w-[16px] h-[19.5px] box-sizing-border">
+                <img src="https://w7.pngwing.com/pngs/141/637/png-transparent-computer-icons-notifications-cdr-area-sound-icon-thumbnail.png" className="w-[16px] h-[19.5px]" />
+              </div>
+              <div className="rounded-[16px] bg-[url('assets/images/Ellipse185.png')] bg-[50%_50%] bg-cover bg-no-repeat w-[32px] h-[32px]">
+              </div>
+            </div>
+          </div>
+          <div className="shadow-[0px_1px_4px_0px_rgba(0,0,0,0.1)] rounded-r-[12px] bg-[#F7F7F7] m-[0_0_24px_0] flex flex-row justify-between self-start p-[10.5px_28.2px_10.5px_24px] w-[335px] box-sizing-border -ml-[8%] ">
+            <span className="m-[0_7.5px_0_0] w-[263.7px] break-words font-['Encode_Sans_Expanded'] font-[var(--textsecondary-title-text-1,600)] text-[12px] text-[#CC2610]">
+            Negative notification type description
+            </span>
+            <div className="m-[1.7px_0_1.7px_0] flex flex-row justify-center w-[11.7px] h-[11.7px] box-sizing-border">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgs1_D_RsTC-uPX430cPiV7Zm70TnvtknvTsSQLrSoNg&s" className="w-[11.7px] h-[11.7px]" />
+            </div>
           </div>
         </div>
       </div>
